8aa9d84aea0a6aa7fa52e0e92d102ab2c7adc1b7
leekheang <ly.kheang.jr@gmail.com>
2019-06-08 19:56:59 +0700

    scapy_http

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,10 +1,12 @@
 import scapy.all as scapy 
+from scapy.layers import http
 
 def sniff(interface):
     scapy.sniff(iface=interface, store = False , prn = process_sniffed_packet )
      # store in memory is false  = save memory , prn callback func (call everytime)
 
 def process_sniffed_packet(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+         print(packet)
 
-sniff("eth0")
+sniff("wlp7s0")
